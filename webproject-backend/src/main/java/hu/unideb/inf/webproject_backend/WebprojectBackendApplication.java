d856ea72e10e56a549ab5aed424e5476ebf3b60f
Szénási Ervin Levente <szenasiocsi@gmail.com>
2024-12-15 09:37:43 +0100

    replaced initial backend
    new backend made through spring initializr

---
+++ webproject-backend/src/main/java/hu/unideb/inf/webproject_backend/WebprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.webproject_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojectBackendApplication.class, args);
+	}
+
+}
